b6e995e32ac5016febb77e1bc6d24c6f11b336eb
CR7-GOAT-REAL <aarav0503@gmail.com>
2025-05-18 16:28:34 +0200

    Add files via upload

---
+++ password_project.py
@@ -0,0 +1,10 @@
+password = "password"
+        
+def generate_password(self): 
+p1 = int(input("Try to guess the password: "))
+
+if password entered correctly:
+    print("Password is correct!")
+
+else:
+print("Password is incorrect!")
